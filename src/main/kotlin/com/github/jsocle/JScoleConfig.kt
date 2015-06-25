5062f22cf3f6ca5fa5aab6551d04f6fe62934caa
vamf12 <vamf12@gmail.com>
2015-06-25 18:20:15 +0900

    change config class as data class

--- src/main/kotlin/com/github/jsocle/JScoleConfig.kt
+++ src/main/kotlin/com/github/jsocle/JScoleConfig.kt
@@ -3,8 +3,7 @@ package com.github.jsocle
 import javax.crypto.spec.SecretKeySpec
 import kotlin.properties.Delegates
 
-public class JScoleConfig {
-    public var secretKey: String = ""
+public data class JScoleConfig(public var secretKey: String = "") {
     public val secretKeySpec: SecretKeySpec by Delegates.lazy {
         if (secretKey == "") {
             throw UnsupportedOperationException("JScoleConfig.secretKey is not defined.")
