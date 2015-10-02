bca45ac5622d37cd32222c87554869ad2f86c445
minek-admin <minek.ceo@gmail.com>
2015-10-02 16:57:00 +0900

    avoid license conflict

--- src/main/kotlin/com/github/jsocle/server/JettyServer.kt
+++ src/main/kotlin/com/github/jsocle/server/JettyServer.kt
@@ -4,6 +4,11 @@ import com.github.jsocle.client.Client
 import com.github.jsocle.client.HttpClient
 import org.eclipse.jetty.server.Server
 
+
+/**
+ * Jetty - Apache License 2.0 + Eclipse Public License
+ */
+
 public class JettyServer(public var port: Int) : Server(port) {
     val client: Client
         get() = HttpClient(port)
