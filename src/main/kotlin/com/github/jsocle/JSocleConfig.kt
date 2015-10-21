f82c43d780376a88ecb6eeb0d6a7ec03696bac14
vamf12 <vamf12@gmail.com>
2015-10-21 15:31:56 +0900

    make JSocleConfig as opend

--- src/main/kotlin/com/github/jsocle/JSocleConfig.kt
+++ src/main/kotlin/com/github/jsocle/JSocleConfig.kt
@@ -1,7 +1,7 @@
 package com.github.jsocle
 
-public class JSocleConfig(secretKey: ByteArray = byteArrayOf()) {
-    public var secretKey: ByteArray = secretKey
+open class JSocleConfig(secretKey: ByteArray = byteArrayOf()) {
+    var secretKey: ByteArray = secretKey
         get() {
             if (field.isEmpty()) {
                 throw UnsupportedOperationException("JSocleConfig.secretKey is not defined.")
